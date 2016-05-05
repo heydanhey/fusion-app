5c1b02b753df584b80a82b40ac3ebe923096c31a
heydanhey <danhey@gmail.com>
2016-05-05 16:45:30 -0500

    added angular

---
+++ app/assets/javascripts/roster_ctrl.js
@@ -0,0 +1,21 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("rosterCtrl", ["$scope", "$http", "$compile", function($scope, $http, $compile)
+    {
+    //   window.scope = $scope;
+
+    //   $scope.setup = function(){
+
+    //     $http.get('/api/v1/lois/' + $scope.activeId + '.json').then(function(response){
+    //       $scope.loi = response.data;
+    //       console.log($scope.loi);
+
+
+    //     });
+      
+    // };
+
+  }]);
+ 
+}());
