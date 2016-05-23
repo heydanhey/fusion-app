bd4ba23f16d33f9cd76061961f9fca1975f9c1c0
heydanhey <danhey@gmail.com>
2016-05-23 12:23:46 -0500

    added functions to disable the selection of multiple players from the same team

--- app/assets/javascripts/player_ctrl.js
+++ app/assets/javascripts/player_ctrl.js
@@ -40,24 +40,39 @@
 
       });
 
-      $scope.updateTeamList = function(team){
-        if (team in $scope.activeTeamList) {
-          return true;
-        } else {
-          $scope.activeTeamList.push(team);
-          return false;
-        };
+
+      //THIS FUNCTION JUST ITERATES THROUGH THE ROSTERS TO HELP GENERATE THE ACTIVE TEAM LIST
+      $scope.addTeams = function(roster){
+        for (var i=0; i<roster.length; i++){
+          if (roster[i].team){
+            $scope.activeTeamList.push(roster[i].team);
+          }
+        }
       }; 
+
+      // THIS FUNCTION JUST KEEPS A LIST CURRENT OF THE TEAMS CURRENTLY SELECTED BY A USER
+      $scope.updateTeamList = function(team){
+        $scope.activeTeamList = [];
+        $scope.addTeams(scope.rosteredQBs);
+        $scope.addTeams(scope.rosteredRBs);
+        $scope.addTeams(scope.rosteredWRs);
+      };
+
+      // maybe try doing normal angular filter with group by and disable for collction select?
 
       $scope.isDisabled = function(player, rosteredPlayer, rosteredPlayers) {
 
-        if (rosteredPlayer.team) {
-          $scope.updateTeamList(rosteredPlayer.team);
-          return false;
-        } else {
+          // CALL FUNCTION TO ADD THE CURRENTLY SELECTED TEAM
+          // THAT WAY  WE CAN DISABLE IF THAT TEAM IS ALREADY SELECTED
 
-          return (rosteredPlayers.indexOf((player)) !== -1 && rosteredPlayers.indexOf((player)) != rosteredPlayers.indexOf(rosteredPlayer))
-        }
+          if (rosteredPlayer.team) {
+            $scope.updateTeamList(rosteredPlayer.team);
+          }
+
+          // THIS WILL RETURN TRUE IF EITHER THAT PLAYER OR HIS TEAM HAS ALREADY BEEEN SELECTED BY THE USER
+
+          return (rosteredPlayers.indexOf((player)) !== -1 && rosteredPlayers.indexOf((player)) != rosteredPlayers.indexOf(rosteredPlayer)) || (rosteredPlayers.indexOf((player)) != rosteredPlayers.indexOf(rosteredPlayer) && $scope.activeTeamList.indexOf(player.team) != -1)
+       
       }; 
 
 
